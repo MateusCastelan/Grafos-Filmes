b582b157765e8de91edb0427bebddcf36f1fcbb4
Mateus <mateus_castelanp@hotmail.com>
2024-04-29 15:24:45 -0300

    ajustada box relacionamento

--- trabalho.js
+++ trabalho.js
@@ -255,7 +255,7 @@ btnBuscar.onclick = function () {
   comprimento.style.display = "block";
   relacionamentos.style.opacity = "1";
   relacionamentos.style.display = "block";
-  relacionamentosBox.style.height = "250px";
+  relacionamentosBox.style.height = "fit-content";
   inputGroup.style.height = "550px";
   btnBuscar.style.marginTop = "15px";
 };
